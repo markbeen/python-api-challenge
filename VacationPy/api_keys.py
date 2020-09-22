06acbaa7349ba507a316eb80f61e06799d5da2b5
mark <markbeen@virginia.edu>
2020-09-21 22:32:28 -0400

    add progress

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -4,4 +4,4 @@ api_key = "9d1bc9c17541af5da45aa33626c8a950"
 #weather_api_key = "f81a98c55421ff91c44c3bc4af84569a"
 
 # Google API Key
-gkey = "AIzaSyAvRuKgD7-ehojwGv-f7Ef6XeLWbvTl6xM"
+g_key = "AIzaSyAvRuKgD7-ehojwGv-f7Ef6XeLWbvTl6xM"
